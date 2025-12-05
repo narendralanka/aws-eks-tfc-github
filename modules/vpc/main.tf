f150b40cc0726f5da0d0c8653ee0d5ec804f38fc
NarendraLanka <narendranadh.lanka@gmail.com>
2025-12-05 16:13:08 +0530

    Add VPC and subnet resources in main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.name}-vpc"
+  }
+}
+
+# Two public + two private subnets (simple POC)
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.cidr_block, 4, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.name}-public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count      = 2
+  vpc_id     = aws_vpc.this.id
+  cidr_block = cidrsubnet(var.cidr_block, 4, count.index + 2)
+
+  tags = {
+    Name = "${var.name}-private-${count.index}"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
