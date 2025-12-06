4eedc63e61438981fb3f268535f2c21d468977cc
NarendraLanka <narendranadh.lanka@gmail.com>
2025-12-06 06:47:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,11 +30,13 @@ module "vpc" {
 }
 
 module "eks" {
-  source       = "./modules/eks"
+  source = "./modules/eks"
+
   cluster_name = "${var.project_name}-eks"
   vpc_id       = module.vpc.vpc_id
-  subnet_ids   = module.vpc.private_subnet_ids
+  subnet_ids   = module.vpc.public_subnet_ids  # <-- use public subnets for nodes
 }
+
 
 
 
